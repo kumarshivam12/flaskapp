58b48dfe0a0eb51fef72dedfd69733518a8878a4
kumarshivam12 <kumarshivam192@gmail.com>
2020-04-21 12:05:00 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,4 @@
-# ------------------------------------------------
-# Program by Denis Astahov
-#
-#
-# Version      Date           Info
-# 1.0          13-Dec-2019    Initial Version
-#
-# ----------------------------------------------
+
 from flask import Flask, render_template
 
 application = Flask(__name__)
