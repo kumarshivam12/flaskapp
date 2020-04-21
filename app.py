05d61f4c40b270e598a363051753cbc4bd305857
kumarshivam12 <kumarshivam192@gmail.com>
2020-04-21 09:59:17 +0530

    updated master

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/")
-def root():
-    return "Hello World from Flask Hello Page"
-if __name__ == '__main__':
-    app.debug= True
-    app.run()
